89c164bb5fab4d74eb9ded32f231bbc8dbbcc79c
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-17 23:55:38 +0530

    Learning Basics

---
+++ FirstProgram.py
@@ -0,0 +1,13 @@
+print("Hello Krishna !!! How are you ?")
+
+# What is character set in python ?
+
+"""Character set in Python is a collection of valid characters that Python can recognize and use in programs."""
+
+"""
+Letters : A to Z and a to z
+Digits : 0 to 9
+Special Symbols : (-,+,*,/,$,@,!,%)
+Whitespaces : Blank space,tab,newline,form feed.  
+"""
+
