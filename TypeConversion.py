1a3f147c9160aa1a86aab2c16c60ad1ae132f889
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-19 20:24:57 +0530

    Type Conversion

---
+++ TypeConversion.py
@@ -0,0 +1,13 @@
+print("Type Conversion ");
+
+# Type conversion is the process of automatically converting one data type into another data type by Python.
+num1=int(input("Enter number : "));
+num2=float(input("Entet number : "));
+
+
+typeConversion=num1+num2;
+
+print(type(typeConversion)); #float
+print(typeConversion); #Actual float value
+
+# Python automatically changes the data type.
