d37f5792d489c2e01763e5f3b58de884aa4bdaff
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-19 19:49:35 +0530

    Understanding User Input

--- FirstProgram.py
+++ FirstProgram.py
@@ -2,12 +2,15 @@ print("Hello Krishna !!! How are you ?")
 
 # What is character set in python ?
 
+# Single Line Comment
 """Character set in Python is a collection of valid characters that Python can recognize and use in programs."""
 
+# Multiline comment
 """
 Letters : A to Z and a to z
 Digits : 0 to 9
 Special Symbols : (-,+,*,/,$,@,!,%)
 Whitespaces : Blank space,tab,newline,form feed.  
 """
+
 
---
+++ UserInput.py
@@ -0,0 +1,8 @@
+print("Print sum of two numbers");
+
+num1=int (input("Enter first number"));
+num2=int (input("Entet second number"));
+print(type(num1));
+print(type(num2));
+sum = num1 + num2;
+print("Sum = ",sum);
