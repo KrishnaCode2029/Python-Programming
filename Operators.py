c7fb8a02df12e0d37ea7b0e095e6a649d1e9f14e
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-19 20:08:12 +0530

    Understanding Operators

---
+++ Operators.py
@@ -0,0 +1,22 @@
+print("Understanding Operators in Python");
+
+# Arithmetic Operator :- +,-,*,/,%;
+number1=int(input("Enter number: "));
+number2=int(input("Enter number : "));
+
+addition=number1+number2;
+subtraction=number1-number2;
+multiplication=number1*number2;
+division=number1/number2;
+remaninder=number1%number2;
+
+print(addition);
+print(subtraction);
+print(multiplication);
+print(division);
+print(remaninder);
+# Assignment Operator :-  +=,-=,*=,/=,**=,%=
+
+# Relational Operator :- >,<,==,>=,<=
+
+# Logical Operator :- not,and,or
