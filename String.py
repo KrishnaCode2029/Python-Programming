66329f3db4ce79a41b257ea267d12388076b35cc
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-20 23:26:32 +0530

    String

---
+++ String.py
@@ -0,0 +1,18 @@
+print("String");
+
+# String is a data type that stores a sequences of characters.
+
+name="Krushna";
+print(type(name));
+
+surname="Govardhane";
+
+# String Concatanation
+print(name + surname);
+
+fullname=name + surname;
+print(fullname);
+
+# print length of string using len()function
+print(len(fullname));
+
