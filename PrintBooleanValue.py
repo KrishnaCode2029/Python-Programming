3abe3a0d6f7837d0f8eed1c3b2729fbb805d9396
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-20 23:14:59 +0530

    Using Boolean values

---
+++ PrintBooleanValue.py
@@ -0,0 +1,12 @@
+print("Print True if a is greater than or equal to b");
+
+a=int(input("Enter a : "));
+b=int(input("Enter b : "));
+
+value=True;
+if a > b :
+   print(value);
+else :
+   value=False;
+   print(value);
+
