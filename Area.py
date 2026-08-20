bb65bc555dedf6502ba9b816cf5778e8d77ff3c4
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-20 23:02:17 +0530

    Area of Square and Area of Rectangle

---
+++ Area.py
@@ -0,0 +1,17 @@
+print("Calculate area of square");
+
+side = int(input("Enter side of square"));
+
+area_of_square=side*side;
+
+print("Area of Square : ",area_of_square);
+
+print("Calculate area of rectangle");
+
+height=float(input("Enter height"));
+width=float(input("Enter width"));
+
+area_of_rectangle=2*(height+width);
+
+print("Area of rectangle is : ",area_of_rectangle);
+
