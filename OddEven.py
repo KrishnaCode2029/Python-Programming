76ebcc0f53932fa37513d9939af9aabac2cee41e
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-24 23:28:59 +0530

    Odd Even Number Identify using conditional statement

---
+++ OddEven.py
@@ -0,0 +1,11 @@
+print("Check Number is even or odd :");
+
+number=int(input("Entet number : "));
+print(number);
+
+if number%2 == 0:
+    print("The number is even number : ");
+elif number%2 == 1:
+    print("The number is odd number : ");
+else:
+    print("The number is  0 ");
