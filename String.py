eed665fdd53392a188f901d9b8e5518b553da4bb
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-24 22:38:30 +0530

    Opertions on String

--- String.py
+++ String.py
@@ -15,4 +15,23 @@ print(fullname);
 
 # print length of string using len()function
 print(len(fullname));
+print(name[0]);
+print(name[1]);
+print(name[2]);
+print(name[3]);
+
+# Accessing Parts of a string
+print(fullname[1:8]);
+print(fullname[:5]);
+
+print(fullname[ :-5]);
+
+str="I am a coder";
+str.endswith("er") #Returns true if string ends with substring
+str.capitalize(); #capitalize 1st char
+# str.replace(a,k);
+str.find("a"); #Return first index of 1st occurance.
+str.count("am") #count the occurrence of substring in string.
+
+
 
---
+++ UserFirstName.py
@@ -0,0 +1,6 @@
+print("WAP to input user's first name and print its length")
+
+name=input("Enter your name");
+print(name);
+
+print(len(name));
