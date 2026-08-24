8a369aca49b1f1a4f17bd12381837bd9ec4ecebf
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-24 22:48:34 +0530

    Change name with surname String question

---
+++ UpdateString.py
@@ -0,0 +1,11 @@
+print("Change name with surname : ");
+
+name=input("Enter name : ");
+print("Name is : ",name);
+
+fullname=input("Enter surname : ");
+print("Fullname is : ",fullname);
+
+name=fullname;
+
+print("Name updated successfully : ",name);
