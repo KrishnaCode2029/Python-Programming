ff0c9b9c037fe3b6644393792d65857ac9c79cdd
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-24 22:54:43 +0530

    Conditional Statement

---
+++ ConditionalStatement.py
@@ -0,0 +1,19 @@
+print("Conditional Statement ");
+
+age=18;
+
+if age >= 18 :
+    print("You can Vote and Drive");
+else :
+    print("You can't Vote and Drive");
+
+
+marks=90;
+if marks > 85:
+    print("Grade A");
+elif marks > 75 and marks < 85:
+  print("Grade B");
+
+elif marks > 55 and marks < 75:
+  print("Grade C");
+
