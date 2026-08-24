d32b79b7763a62c577bb03934324727473cbb263
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-24 23:41:15 +0530

    Finding greatest number using conditional statement

---
+++ FindGreatest.py
@@ -0,0 +1,13 @@
+print("Print Greatest ");
+
+n1=int(input("Enter first number : "));
+n2=int(input("Enter second number : "));
+n3=int(input("Enter third number : "));
+
+if n1 >= n2 and n1>=n3:
+   print(n1," Is Greatest Number");
+elif n2>=n1 and n2>=n3:
+  print(n2," Is Greatest Number");
+else:
+  print(n3," Is Greatest Number");
+
