aa16978f33554ae6cab55613bd69cf329f02feae
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-25 22:26:36 +0530

    Divisible by input or not using conditional statement

---
+++ DivisibleByInput.py
@@ -0,0 +1,10 @@
+# Check number is divisible by input 
+
+number=int(input("Enter number: "))
+divisible=int((input("Enter divisible")))
+
+if number%divisible==0:
+    print("The number",number," is divisible by ", divisible)
+else:
+    print("The number is not divisble.")
+
--- Product.py
+++ Product.py
@@ -7,3 +7,11 @@ n3=int(input("Enter third number"));
 product=n1*n2*n3;
 
 print("Product of 3 numbers : ",product);
+
+print("Product of 3 numbers")
+n1=37
+n2=61
+n3=391
+
+Product=n1*n2*n3
+print("Product of 3 numbers : ",Product)
